862e81323995a178c4a83b031b6e31c4be92777c
Telity <gunniko@gmail.com>
2024-09-03 11:02:33 +0200

    Tourist

--- src/main/java/kea/gruppeseven/turistguide/Model/TouristAttraction.java
+++ src/main/java/kea/gruppeseven/turistguide/Model/TouristAttraction.java
@@ -1,4 +1,24 @@
 package kea.gruppeseven.turistguide.Model;
 
 public class TouristAttraction {
+
+    private String name;
+    private String description;
+
+    public TouristAttraction(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/kea/gruppeseven/turistguide/Repository/TouristRepository.java
+++ src/main/java/kea/gruppeseven/turistguide/Repository/TouristRepository.java
@@ -1,4 +1,19 @@
 package kea.gruppeseven.turistguide.Repository;
 
+import kea.gruppeseven.turistguide.Model.TouristAttraction;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TouristRepository {
+    private final List<TouristAttraction> attractions = new ArrayList<>();
+
+    public TouristRepository(){
+        populateAttractions();
+    }
+    private void populateAttractions() {
+        attractions.add(new TouristAttraction("Rundetårn","Højt tårn"));
+        attractions.add(new TouristAttraction("Eiffel Tower", "Tower in paris"));
+        attractions.add(new TouristAttraction("Lille havfrue","Figur"));
+    }
 }
